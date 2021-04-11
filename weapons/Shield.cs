828e4cf17058fca54bbbfce95a989180f264a57e
Danil Ko <danilko@localhost.localdomain>
2021-04-10 20:08:03 -0700

    https://github.com/danilko/topdown-2d-multiplayer/issues/7: Update shield logic to force disable if ammo used up, and shield will detect ammo when block attacks base on attack's damage

--- weapons/Shield.cs
+++ weapons/Shield.cs
@@ -34,7 +34,6 @@ public class Shield : Weapon
         GetGameWorld().RemoveChild(_shieldPhysics);
         
         _shieldPhysics.QueueFree();
-        GD.Print("QUEUE FREE");
         base.Deinitialize();
     }
 
